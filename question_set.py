a9963294aa511dc812bc4196136b6dc882e17cb7
Ga3ta <carlos.gaeta.lopez@gmail.com>
2020-10-12 11:39:43 -0500

    Cambió el rango de random_num (línea 11)

--- question_set.py
+++ question_set.py
@@ -8,7 +8,7 @@ def throw_questions():
         e = line.split(",")
         question_data.append({"name": e[0], "symbol": e[1], "number": int(e[2])})
 
-    random_num=random.randrange(1,118)
+    random_num=random.randrange(0,len(e))
     big_var= question_data[random_num]["symbol"]
     lil_var = question_data[random_num]["number"]
     name_var = question_data[random_num]["name"]
