a1b73f04237b3eb752451ff86f44d44dcb7f6ccf
Ga3ta <carlos.gaeta.lopez@gmail.com>
2020-10-19 10:01:21 -0500

    C añadió la ortografía

---
+++ ortografia.py
@@ -0,0 +1,14 @@
+from random import randint
+
+def get_palabra():
+    file = open("assets/diccionario.txt", "r", encoding='utf-8')
+    palabras = file.read().split("\n")
+    rand = randint(0, len(palabras)-1)
+    palabra=palabras[rand].split(",")
+    size = len(palabra)//2
+    rand2 = randint(0, size-1)
+    correct = palabra[rand2]
+    description = palabra[rand2+size]
+    bank = palabra[:size]
+    print(correct, description, bank)
+get_palabra()
